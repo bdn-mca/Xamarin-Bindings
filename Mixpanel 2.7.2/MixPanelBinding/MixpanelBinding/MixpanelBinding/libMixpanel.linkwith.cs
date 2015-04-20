2144d37a338e4e633ce73df09f7bc76e59ef2d91
bdn-mca <bdn@mca.mk>
2015-04-20 09:59:03 +0200

    - Added Xamarin Binding project and source files for XCode project

---
+++ Mixpanel 2.7.2/MixPanelBinding/MixpanelBinding/MixpanelBinding/libMixpanel.linkwith.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using ObjCRuntime;
+
+[assembly: LinkWith (
+    "libMixpanel.a",
+    LinkTarget = LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64,
+    Frameworks = "MapKit ImageIO Accelerate Security CFNetwork AdSupport CoreGraphics CoreTelephony Foundation QuartzCore SystemConfiguration UIKit",
+    LinkerFlags = "-ObjC -licucore",
+    SmartLink = true,
+    ForceLoad = true
+)]
